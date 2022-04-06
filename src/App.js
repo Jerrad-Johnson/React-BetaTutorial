6e5f8b77b3328c670d2c7985cafd653b5a526ab2
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-04-06 17:21:02 -0600

    Pg. Quick Start.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+let x = 55;
+let y = Number(4);
+
+/*if (isLoggedIn) {
+  let loginPanel = <ComponentReference/>
+} else {
+  let loginPanel = <ComponentReference2/>
+}*/
+
+let cc = console.log;
+let printMe;
+let stuff = true;
+
+if (stuff) {
+  printMe = "All the stuff";
+} else {
+  printMe = "None of the stuff";
+}
+
+
+const urls = [
+  { title: "Home", url: "./", key: 0 },
+  { title: "About", url: "./About", key: 1 },
+  { title: "Contact", url: "./Contact", key: 2 },
+];
+
+const urlsMap = urls.map(url =>
+<a href={url.url} key={url.key}> {url.title} </a> );
+/*
+function MyButton() {
+    const [count, setCount] = useState(2);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+    return (
+        <div>
+            <button onClick={handleClick}>TOUCH ME {count} TIMES!</button>
+        </div>
+    )
+}
+*/
+
+function MyButton({count}) {
+    return (
+        <button>{count}</button>
+    )
+}
 
 function App() {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+
+    function locations() {
+        return;
+    }
+
+
+
+    const where = []
+    where[0] = 5;
+    let aL = where[0];
+    let bL = locations[1];
+
+    cc(aL);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*{ (5 > 4) ? alert("yes") : alert("no") } Runs twice */}
+      {/*{ x ? {x} : 3} Doesn't work. */}
+      {x}{y}
+      <br />
+      {x + y}
+      <br />
+      {printMe}
+      <br />
+      {urlsMap}
+      <br />
+        <button onClick={handleClick}>Click Me NOW! Do it {count} more times!</button>
+        <button onClick={handleClick}>Click Me NOW! Do it {count} more times!</button>
+        <br />
+        <MyButton count={5}></MyButton>
+        <MyButton count={5}></MyButton>
+      <br />
+        {/*<MyButton />
+        <MyButton />*/}
+
     </div>
   );
 }
