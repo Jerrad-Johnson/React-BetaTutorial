241f02ae2f1a7ef443fe4a7613ed669113493a80
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-04-07 21:37:47 -0600

    Finished Thinking in React tutorial, and created a functional search bar.

--- src/ThinkingInReact.js
+++ src/ThinkingInReact.js
@@ -1,74 +1,136 @@
 import './App.css';
-// import {useState} from "react";
+import {memo, useState} from "react";
 
-let cc = console.log;
-
-function QueryTable(){
+function QueryTable({familyMembers}){
     let inputKey = "familySearchbar"
     let tableDesignKey = "familyMembers"
-    cc(FAMILYMEMBERS);
-    alert(5);
+    let [searchValue, setSearchValue] = useState("");
 
     return (
         <div className={"tableContainer"}>
             <h2>Table of My Family Members.</h2>
             <SearchBar
-                inputKey = {inputKey} />
-            <FamilyTableEntries
-                tableDesignKey = {tableDesignKey} />
+                inputKey = {inputKey}
+                searchValue = {searchValue}
+                onSearchValueChange = {setSearchValue}
+            />
+            <TableEntries
+                tableDesignKey = {tableDesignKey}
+                familyMembers = {familyMembers}
+                searchValue = {searchValue}
+            />
         </div>
     );
 }
 
-function SearchBar(inputKey){
+function SearchBar({inputKey, searchValue, onSearchValueChange}){
     return(
       <div>
           <form>
-              <label>Name</label><br />
-              <input type={"text"} className={{inputKey} + " searchBar"} />
+              <label>Search by Name</label><br />
+              <input
+                  type={ "text"}
+                  className={inputKey + " searchBar"}
+                  value={searchValue}
+                  onChange={(e) =>
+                      onSearchValueChange(e.target.value)
+                  }
+              />
           </form>
       </div>
     );
 }
 
-function FamilyTableEntries(tableDesignKey){
+function CreateTableHead({familyMembers}){
+    let headers = Object.keys(familyMembers[0]);
+    const tableHeader = [];
+
+    headers.forEach((header) => {
+        tableHeader.push(
+            <th key={header}>{header}</th>
+        );
+    });
+
+    return (
+        <thead>
+            <tr>
+                {tableHeader}
+            </tr>
+        </thead>
+    );
+}
+
+function CreateTableRows({familyMembers, searchValue}){
+    const rows = [];
+    let removeCaseFromSearchValue = searchValue.toLowerCase();;
+    let removeCaseFromFamilyMemberList = [];
+
+    /*familyMembers.forEach(member => {
+
+    });*/
+
+
+
+    if (searchValue == ""){
+        familyMembers.forEach(member => {
+            rows.push(
+                <tr key={member.name}>
+                    <td>{member.name}</td>
+                    <td>{member.relation}</td>
+                    <td>{member.degree}</td>
+                </tr>
+            );
+        });
+    } else {
+        familyMembers.forEach(member => {
+            if (member.name.toLowerCase() == removeCaseFromSearchValue){
+                rows.push(
+                    <tr key={member.name}>
+                        <td>{member.name}</td>
+                        <td>{member.relation}</td>
+                        <td>{member.degree}</td>
+                    </tr>
+                );
+            }
+        });
+    }
+
+   return(<tbody>
+            {rows}
+       </tbody>
+   );
+}
+
+function TableEntries({tableDesignKey, familyMembers, searchValue}){
     return(
         <div>
-            <table className={{tableDesignKey}}>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Degree</th>
-                        <th>Relation</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Amy</td>
-                        <td>First</td>
-                        <td>Mother</td>
-                    </tr>
-                </tbody>
+            <table className={tableDesignKey}>
+                    <CreateTableHead
+                        familyMembers = {familyMembers} />
+                    <CreateTableRows
+                        familyMembers = {familyMembers}
+                        searchValue = {searchValue}
+                    />
             </table>
         </div>
     );
 }
 
 const FAMILYMEMBERS = [
-    { degree: "First", name: "Amy", relation: "Mother", },
-    { degree: "First", name: "Brent", relation: "Father", },
-    { degree: "Second", name: "Tommy", relation: "Brother", },
-    { degree: "Second", name: "Ashton", relation: "Brother", },
-    { degree: "Second", name: "Lexy", relation: "Sister", },
-    { degree: "Second", name: "Hannah", relation: "Sister", },
-    { degree: "Second", name: "Abby", relation: "Sister", },
+    { name: "Amy", relation: "Mother", degree: "First",},
+    { name: "Brent", relation: "Father", degree: "First", },
+    { name: "Tommy", relation: "Brother", degree: "Second", },
+    { name: "Ashton", relation: "Brother",  degree: "Second", },
+    { name: "Lexy", relation: "Sister",  degree: "Second", },
+    { name: "Hannah", relation: "Sister",  degree: "Second", },
+    { name: "Abby", relation: "Sister",  degree: "Second", },
 ]
 
 function ThinkingInReact() {
     return (
         <div className="TIR">
             <QueryTable
-                FAMILYMEMBERS = {FAMILYMEMBERS}/>
+                familyMembers = {FAMILYMEMBERS}/>
         </div>
     );
 }
